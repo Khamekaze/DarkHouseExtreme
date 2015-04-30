69ccd1f1abd693bf596ea9b9d5caabb8efc2701a
Mattias <mattias_sonemar@hotmail.com>
2015-04-30 10:43:23 +0200

    fixed databasetest

---
+++ app/src/androidTest/java/com/bam/darkhouseextreme/app/TestOfDatabase.java
@@ -0,0 +1,44 @@
+package com.bam.darkhouseextreme.app;
+
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+import com.bam.darkhouseextreme.app.helper.DatabaseHelper;
+import com.bam.darkhouseextreme.app.model.Player;
+
+/**
+ * Created by Chobii on 30/04/15.
+ */
+
+
+public class TestOfDatabase extends AndroidTestCase {
+
+    private Player player;
+    private DatabaseHelper helper;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "_test");
+        helper = new DatabaseHelper(context);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testCRUD() {
+        // Test Add.
+        String name = "Akana";
+        player = helper.createCharacter(name);
+        assertEquals(name, player.getName());
+        // Test Update.
+        boolean updated = helper.updateCharacter(String.valueOf(player.getId()), "0", "1", 100);
+        assertTrue(updated);
+        // Test Read.
+        // Test Delete.
+        boolean deleted = helper.deleteCharacter(String.valueOf(player.getId()));
+        assertTrue(deleted);
+
+    }
+}
